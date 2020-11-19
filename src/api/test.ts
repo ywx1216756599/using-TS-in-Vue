fb65b4172b2d71c83feb62976f41e024114ae602
小皮果 <1216756599@qq.com>
2020-11-19 10:36:57 +0800

    test

---
+++ src/api/test.ts
@@ -0,0 +1,6 @@
+import httpServer from '@/api'
+
+export const test1 = () => httpServer.instance({
+  method: 'GET',
+  url: 'http://127.0.0.1:2000/api/demo',
+})
---
+++ src/router/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from './interfaceList'
+
+const routes: Array<Router> = [
+]
+
+export default routes
+
